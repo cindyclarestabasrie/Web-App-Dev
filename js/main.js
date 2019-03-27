ace26ff6ac7012ab71bfeea76f2342bd7f8c7166
Jordy Filbert <jordyfilbert3@gmail.com>
2019-03-27 11:16:41 +0700

    Fixed login

--- js/main.js
+++ js/main.js
@@ -2,4 +2,13 @@ window.onload = () => {
     if (localStorage.getItem('token')) {
         document.getElementById('login-btn').style.display = 'none';
     }
+
+    if (!localStorage.getItem('token')) {
+        document.getElementById('logout-btn').style.display = 'none';
+    }
 };
+
+function logout() {
+    localStorage.removeItem('token');
+    location.reload();
+}
