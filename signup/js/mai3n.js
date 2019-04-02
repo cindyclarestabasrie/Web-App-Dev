65807de3a9484d5d0327e76d6a1f64754e6135c2
Jordy <jordyfilbert3@gmail.com>
2019-04-02 14:08:49 +0800

    Test signupfix

--- signup/js/mai3n.js
+++ signup/js/mai3n.js
@@ -4,30 +4,51 @@ function register() {
     var name = document.getElementById('name').value,
         gender = document.getElementById('gender').value,
         address = document.getElementById('address').value,
-        phonenum = document.getElementById('phonenum').value,
+        phone = document.getElementById('phone').value,
+        date_of_birth = document.getElementById('date_of_birth').value,
         email = document.getElementById('email').value,
         password = document.getElementById('pass').value;
 
     var datalogin = {
         email: email,
         password: password,
-        phonenum: phonenum,
+        role_id: 2
+    };
+
+    var datausers = {
+        name: name,
+        phone: phone,
         address: address,
         gender: gender,
-        name: name,
-        role_id: 2
-       
+        date_of_birth: date_of_birth
+
     };
-    
 
 
+    console.log(JSON.stringify(datalogin));
 
     $.ajax({
         method: 'post',
         url: 'http://178.128.82.158/api/login',
+        // url: 'http://localhost:2000/api/login',
         contentType: 'application/json',
         dataType: 'json',
         data: JSON.stringify(datalogin),
+        success: function(response) {
+            localStorage.setItem('token', response.access_token);
+        },
+        error: function(response) {
+            console.log(response);
+        }
+    })
+
+    $.ajax({
+        method: 'post',
+        url: 'http://178.128.82.158/api/user',
+        // url: 'http://localhost:2000/api/user',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify(datausers),
         success: function(response) {
             localStorage.setItem('token', response.access_token);
         },
